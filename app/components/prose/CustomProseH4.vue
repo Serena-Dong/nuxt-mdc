1259808d471aa45849dfce6a1225a49fc51621d2
Serena Dong <serena.dong@atoms.retex.com>
2025-04-10 17:55:59 +0200

    feat: add custom components (to be customized)

---
+++ app/components/prose/CustomProseH4.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{ id?: string }>();
+
+const { headings } = useRuntimeConfig().public.mdc;
+const generate = computed(
+  () =>
+    props.id &&
+    ((typeof headings?.anchorLinks === "boolean" &&
+      headings?.anchorLinks === true) ||
+      (typeof headings?.anchorLinks === "object" && headings?.anchorLinks?.h1))
+);
+</script>
+
+<template>
+  <h4 :id="props.id">
+    <a v-if="generate" :href="`#${props.id}`">
+      <slot />
+    </a>
+    <slot v-else />
+  </h4>
+</template>
