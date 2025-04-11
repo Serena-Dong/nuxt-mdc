55c1135a8813a2a4ddf4ea7149c9e6420486ba14
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-11 11:21:55 +0200

    chore: upgrade homepage copy

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,2 +1,7 @@
 <script></script>
-<template><div class="!p-4 md:!p-8 md:!pb-4">Cards</div></template>
+<template>
+  <div class="!p-4 md:!p-8 md:!pb-4">
+    <h1>Welcome to my blog</h1>
+    <h2>Posts list</h2>
+  </div>
+</template>
