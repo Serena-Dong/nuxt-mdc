1ff13a8a171a74be74dec9b10127b223df8dec7d
Serena Dong <serena.dong@atoms.retex.com>
2025-04-14 18:05:00 +0200

    feat: add alert snippet

---
+++ app/components/mdc/Snippet.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    required: true,
+    type: String,
+  },
+});
+
+const snippets = computed(() => {
+  const snippetMap = {
+    alert: `# I'm an alert snippet
+        - Alert 1
+        - Alert 2
+    `,
+    calendar: `# I'm a calendar snippet`,
+  };
+
+  return snippetMap[props.name];
+});
+</script>
+<template>
+  <MDC :body="snippets" />
+</template>
