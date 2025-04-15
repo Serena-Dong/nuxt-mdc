f1d5dbca3bae9dec0a4007b7cb375560cb9f0f20
Serena Dong <serena.dong@atoms.retex.com>
2025-04-15 18:56:02 +0200

    feat: add todo

--- app/components/mdc/CustomButton.vue
+++ app/components/mdc/CustomButton.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   color: {
     type: String,
     default: "red",
   },
 });
+
+// ToDo: to be implemented a new system to assign classes to the button
+const buttonClass = computed(() => {
+  return `bg-${props.color}-500`;
+});
 </script>
 <template>
-  <NuxtLink :class="`custom-button bg-${color}-500 py-2 px-4`"
+  <NuxtLink :class="buttonClass + `custom-button py-2 px-4 `"
     ><slot
   /></NuxtLink>
 </template>
