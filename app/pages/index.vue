20511c6c30012ed079e84160ee4d0ea62c2494cb
Serena Dong <serena.dong@atoms.retex.com>
2025-04-15 18:53:50 +0200

    fix: alert snippet removed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-const { data } = await useFetch("/api/getSnippet/alert");
-</script>
+<script setup lang="ts"></script>
 <template>
   <div class="!p-4 md:!p-8 md:!pb-4">
     <h1>Welcome to my blog</h1>
     <h2>Posts list</h2>
-    <pre>{{ data }}</pre>
   </div>
 </template>
