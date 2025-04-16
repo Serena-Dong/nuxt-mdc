7202bf7e88e31aca64dab14a09f783a7baf25193
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-16 16:27:45 +0200

    refactor: move snippets into assets/snippets directory

--- server/api/getSnippet/[name].get.ts
+++ server/api/getSnippet/[name].get.ts
@@ -4,7 +4,7 @@ import { readFileSync } from "fs";
 export default defineEventHandler(async (event) => {
   const { name } = getRouterParams(event);
 
-  const filePath = resolve(join("app/assets/content", `${name}.md`));
+  const filePath = resolve(join("app/assets/snippets", `${name}.md`));
   const fileContent = readFileSync(filePath, "utf-8");
 
   return fileContent;
