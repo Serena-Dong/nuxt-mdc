742a5643c811b5c0fdeaf8d8120dfd096c5bbf82
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-17 17:30:29 +0200

    feat: add section

--- app/components/mdc/CustomTemplate.vue
+++ app/components/mdc/CustomTemplate.vue
@@ -1,10 +1,10 @@
 <template>
-  <div
+  <section
     class="custom-template p-4 md:p-8 md:pb-4 md:px-1/10 lg:px-1/7 xl:px-1/4 bg-amber-200 min-h-full"
   >
     <div class="mx-auto max-w-200 bg-white">
       <MDCSlot name="title" />
       <MDCSlot name="content" />
     </div>
-  </div>
+  </section>
 </template>
