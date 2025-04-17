c83ba430e70b38173cb890f8491e2c4b5818ed40
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-17 13:25:59 +0200

    feat: add BlogCard props type + date handling

---
+++ app/components/Molecules/BlogCard.props.ts
@@ -0,0 +1,7 @@
+export type BlogCardProps = {
+  title: string;
+  description: string;
+  slug: string;
+  /** The ISO string of the creation date  */
+  date: string;
+};
--- app/components/Molecules/BlogCard.vue
+++ app/components/Molecules/BlogCard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-defineProps<{
-  title: string;
-  description: string;
-  slug: string;
-}>();
+import type { BlogCardProps } from "./BlogCard.props";
+const props = defineProps<BlogCardProps>();
+
+// Parse the date string to a Date object
+const parsedDate = computed(() => new Date(props.date));
 </script>
 
 <template>
@@ -11,6 +11,11 @@ defineProps<{
     class="border border-gray-200 rounded-lg p-4 mb-4 hover:bg-gray-50 transition-colors duration-200"
   >
     <NuxtLink :to="`/posts/${slug}`" class="block">
+      <p class="text-sm text-right">
+        {{
+          `${parsedDate.getDay()}-${parsedDate.getMonth()}-${parsedDate.getFullYear()}`
+        }}
+      </p>
       <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
       <p class="text-gray-600 line-clamp-4 truncate">
         <Suspense suspensible>
