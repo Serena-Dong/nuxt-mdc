77936524094395eacc9c18f0c0c283ed532075e8
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-17 14:40:47 +0200

    feat: add CustomTemplate styling

--- app/components/mdc/CustomTemplate.vue
+++ app/components/mdc/CustomTemplate.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="custom-template">
-    <MDCSlot name="title" />
-    <MDCSlot name="content" />
+  <div
+    class="custom-template p-4 md:p-8 md:pb-4 md:px-1/10 lg:px-1/7 xl:px-1/4 bg-amber-200 min-h-full"
+  >
+    <article id="main-content" class="mx-auto max-w-200 bg-white">
+      <MDCSlot name="title" />
+      <MDCSlot name="content" />
+    </article>
   </div>
 </template>
