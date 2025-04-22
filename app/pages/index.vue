26ca093fcdc506e4256c6910b38694dc31f56fcf
Serena Dong <serena.dong@atoms.retex.com>
2025-04-22 12:57:37 +0200

    feat: blog card redesign

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,12 +3,7 @@ const { data: posts } = await useFetch("/api/getMarkdown", { method: "GET" });
 </script>
 <template>
   <div class="!p-4 md:!p-8 md:!pb-4 min-h-full">
-    <h1>Welcome to my blog</h1>
-    <h2>Posts list</h2>
-    <div
-      v-if="posts?.length"
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-5"
-    >
+    <div v-if="posts?.length" class="">
       <MoleculesBlogCard v-for="post in posts" :key="post.slug" v-bind="post" />
     </div>
     <div v-else class="py-5 text-center">
