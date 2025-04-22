69b20386763ff4968a67c6b6a062f3ece4c1fd05
Serena Dong <serena.dong@atoms.retex.com>
2025-04-22 12:12:09 +0200

    feat: add nuxt font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/image", "@nuxt/icon", "@nuxtjs/mdc"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxtjs/mdc",
+    "@nuxt/fonts",
+  ],
   future: { compatibilityVersion: 4 },
   css: ["~/assets/css/main.css"],
   fonts: {
