33b7513865479b6ee0c215f452bd53a57e610bdd
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-04-30 18:16:16 +0200

    feat: add true asyc component test

---
+++ app/assets/content/async-component-test.md
@@ -0,0 +1,6 @@
+Here we will try to render a table component containing asynchronous data
+
+To make sure that the async component is rendered we need to wrap the `<MDC>` component inside `<Suspense suspensible>`
+
+::user-list-table
+::
---
+++ app/components/mdc/UserListTable.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+type UserData = {
+  name: string;
+  username: string;
+  email: string;
+};
+
+const { data: users } = await useFetch<UserData[]>(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
+
+<template>
+  <table class="w-full" v-if="users?.length">
+    <tr v-for="user in users">
+      <td class="border-b-2 border-gray-400 py-2">
+        <p class="text-bold text-black">{{ user.name }}</p>
+      </td>
+      <td class="border-b-2 border-gray-400 py-2">
+        <p class="text-bold text-black">{{ user.username }}</p>
+      </td>
+      <td class="border-b-2 border-gray-400 py-2">
+        <p class="text-bold text-black">{{ user.email }}</p>
+      </td>
+    </tr>
+  </table>
+  <div v-else><p>UserListTable contains no users</p></div>
+</template>
