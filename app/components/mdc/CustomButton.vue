fb5b2a7354971d31291736d42d74b0c56d51ec2b
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-05-06 12:06:08 +0200

    fix: fix CustomButton component

--- app/components/mdc/CustomButton.vue
+++ app/components/mdc/CustomButton.vue
@@ -6,13 +6,23 @@ const props = defineProps({
   },
 });
 
-// ToDo: to be implemented a new system to assign classes to the button
 const buttonClass = computed(() => {
-  return `bg-${props.color}-500`;
+  switch (props.color) {
+    case "green":
+      return "bg-green-500 hover:bg-green-600";
+    case "blue":
+      return "bg-blue-500 hover:bg-blue-600";
+    case "yellow":
+      return "bg-yellow-500 hover:bg-yellow-600";
+    case "red":
+      return "bg-red-500 hover:bg-red-600";
+    default:
+      return "bg-gray-500 hover:bg-gray-600";
+  }
 });
 </script>
 <template>
-  <NuxtLink :class="buttonClass + `custom-button py-2 px-4 `"
-    ><MDCSlot
-  /></NuxtLink>
+  <button :class="[buttonClass, `custom-button py-2 px-4 rounded`]">
+    <MDCSlot />
+  </button>
 </template>
