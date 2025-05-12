51595477f094753b2934bfbe8c1b421d35b300f3
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-05-12 15:22:27 +0200

    refactor: fix NewSnippetForm and NewPostForm form value types

--- app/components/Organisms/NewPostForm.vue
+++ app/components/Organisms/NewPostForm.vue
@@ -1,47 +1,47 @@
 <script setup lang="ts">
-import type { NewPostFormValues } from "./NewPostForm.props";
+import type { NewPostFormValues } from './NewPostForm.props'
 
 const emit = defineEmits<{
-  (e: "submit", formValues: NewPostFormValues): void;
-}>();
-const formValues = defineModel<NewPostFormValues>("newPostForm", {
+  (e: 'submit', formValues: NewPostFormValues): void
+}>()
+const formValues = defineModel<NewPostFormValues>('newPostForm', {
   required: true,
-});
+})
 
 const slugPlaceholderValue = computed(() => {
-  const kebabCaseTitle = kebabify(formValues.value.title ?? "");
+  const kebabCaseTitle = kebabify(formValues.value.title ?? '')
 
   return formValues.value.title?.length
     ? kebabCaseTitle
-    : "Inserisci uno slug univoco per il tuo post";
-});
+    : 'Inserisci uno slug univoco per il tuo post'
+})
 const submitCleanup = (formBody: NewPostFormValues) => {
   if (!formBody.title?.length || !formBody.content?.length) {
-    alert("Missing post title or content");
-    return;
+    alert('Missing post title or content')
+    return
   }
 
   const validatedFormBody = {
     title: formBody.title?.trim(),
     content: formBody.content?.trim(),
     slug: formBody.slug?.length ? formBody.slug : kebabify(formBody.title),
-  };
+  }
 
-  emit("submit", validatedFormBody);
-};
+  emit('submit', validatedFormBody)
+}
 
 const addNewLineOnEnter = (e: KeyboardEvent) => {
-  const target = e.target as HTMLTextAreaElement;
+  const target = e.target as HTMLTextAreaElement
 
-  let cursorPosition = target.selectionEnd;
+  const cursorPosition = target.selectionEnd
 
   target.value =
     target.value.substring(0, cursorPosition) +
-    "\n" +
-    target.value.substring(cursorPosition);
+    '\n' +
+    target.value.substring(cursorPosition)
 
-  target.selectionEnd = cursorPosition + 1;
-};
+  target.selectionEnd = cursorPosition + 1
+}
 </script>
 
 <template>
@@ -54,7 +54,7 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
           type="text"
           v-model="formValues.title"
           placeholder="Title"
-          class="border border-gray-300 rounded p-2 mb-4 w-full"
+          class="mb-4 w-full rounded border border-gray-300 p-2"
         />
       </div>
       <div class="slug flex-1/2">
@@ -65,11 +65,11 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
           disabled
           v-model="formValues.slug"
           :placeholder="slugPlaceholderValue"
-          class="border border-gray-300 rounded p-2 mb-4 w-full"
+          class="mb-4 w-full rounded border border-gray-300 p-2"
         />
       </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
       <div class="col-span-1">
         <label for="new-post__content" class="text-lg font-semibold">
           Content
@@ -78,10 +78,10 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
           id="new-post__content"
           v-model="formValues.content"
           placeholder="Write your post content in markdown"
-          class="border border-gray-300 rounded p-2 mb-4 w-full h-40 overflow-y-scroll align-text-top whitespace-pre-wrap"
+          class="mb-4 h-40 w-full overflow-y-scroll rounded border border-gray-300 p-2 align-text-top whitespace-pre-wrap"
           @keydown.enter.prevent="addNewLineOnEnter"
         />
-        <div class="text-xs flex flex-col gap-3">
+        <div class="flex flex-col gap-3 text-xs">
           <p>
             <i>
               <b>Tip:</b> You can use the following shortcuts to insert snippets
@@ -119,7 +119,7 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
         <div
           id="new-post__content__preview"
           readonly
-          class="border border-gray-300 rounded p-2 mb-4 w-full h-40 overflow-y-scroll"
+          class="mb-4 h-40 w-full overflow-y-scroll rounded border border-gray-300 p-2"
         >
           <MDC
             v-if="(formValues.content ?? '').length"
@@ -133,7 +133,7 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
     </div>
     <button
       type="submit"
-      class="bg-black uppercase text-white cursor-pointer self-end py-2 px-4 hover:bg-gray-800"
+      class="cursor-pointer self-end bg-black px-4 py-2 text-white uppercase hover:bg-gray-800"
     >
       Save
     </button>
