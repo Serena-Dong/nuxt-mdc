da1a1c247181dc10ac379bf5310ec65c8e62f8f2
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-05-12 18:20:20 +0200

    fix: adds fix to homepage navigation

--- app/pages/new-post.vue
+++ app/pages/new-post.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import type { NewSnippetFormValues } from "~/components/Organisms/NewSnippetForm.props";
-import type { NewPostFormValues } from "~/components/Organisms/NewPostForm.props";
+import type { NewSnippetFormValues } from '~/components/Organisms/NewSnippetForm.props'
+import type { NewPostFormValues } from '~/components/Organisms/NewPostForm.props'
 //SNIPPET LIST
-const { data: snippets } = await useFetch("/api/snippets");
-const { data: inlineSnippets } = await useFetch("/api/snippets?inline=true");
+const { data: snippets } = await useFetch('/api/snippets')
+const { data: inlineSnippets } = await useFetch('/api/snippets?inline=true')
 
 // NEW POST FORM
 const newPostData = ref<NewPostFormValues>({
-  title: "",
-  slug: "",
-  content: "",
-});
+  title: '',
+  slug: '',
+  content: '',
+})
 // New Snippet Form
 const newSnippetForm = ref<NewSnippetFormValues>({
-  name: "",
+  name: '',
   inline: false,
-  content: "",
-});
+  content: '',
+})
 
 // const feedbackPopup = ref<PopupProps>({
 //   status: "",
@@ -25,56 +25,56 @@ const newSnippetForm = ref<NewSnippetFormValues>({
 // const showPopup = ref(false);
 
 const writeNewPost = async (submitPayload: NewPostFormValues) => {
-  newPostData.value = { ...submitPayload };
-  console.log("New post data:", newPostData.value);
+  newPostData.value = { ...submitPayload }
+  console.log('New post data:', newPostData.value)
 
-  await $fetch("/api/posts", {
-    method: "POST",
+  await $fetch('/api/posts', {
+    method: 'POST',
     body: {
       title: newPostData.value.title,
       slug: newPostData.value.slug,
       content: newPostData.value.content,
     },
   })
-    .then((response) => {
-      console.log("Post created successfully:", response);
+    .then(response => {
+      console.log('Post created successfully:', response)
       //   showPopup.value = true;
       //   feedbackPopup.value = {
       //     status: "success",
       //     content: "Post created successfully!",
       //   };
-      navigateTo("/");
+      navigateTo('/')
     })
-    .catch((error) => {
-      console.error("Error creating post:", error);
+    .catch(error => {
+      console.error('Error creating post:', error)
       //   showPopup.value = true;
       //   feedbackPopup.value = {
       //     status: "error",
       //     content: "Error creating post. Please try again.",
       //   };
-    });
-};
+    })
+}
 
 // Sidebar Function
-const showSnippetList = ref(false);
-const showCreateSnippet = ref(false);
+const showSnippetList = ref(false)
+const showCreateSnippet = ref(false)
 
 const toggleSnippetList = () => {
-  showSnippetList.value = !showSnippetList.value;
-};
+  showSnippetList.value = !showSnippetList.value
+}
 const toggleCreateSnippet = () => {
-  showCreateSnippet.value = !showCreateSnippet.value;
-};
+  showCreateSnippet.value = !showCreateSnippet.value
+}
 </script>
 
 <template>
   <div class="min-h-full p-4 pt-0 md:p-8 md:!pt-0">
     <!-- Main content area -->
     <section id="new-post-form" class="flex flex-col gap-6">
-      <div class="flex justify-between items-center">
-        <h2 class="md:text-2xl text-lg uppercase">Create a new post</h2>
+      <div class="flex items-center justify-between">
+        <h2 class="text-lg uppercase md:text-2xl">Create a new post</h2>
         <button
-          class="bg-black text-white cursor-pointer text-sm self-end py-2 px-4 hover:bg-gray-800 uppercase"
+          class="cursor-pointer self-end bg-black px-4 py-2 text-sm text-white uppercase hover:bg-gray-800"
           @click="toggleSnippetList"
         >
           Snippet List
@@ -86,52 +86,51 @@ const toggleCreateSnippet = () => {
       />
     </section>
 
-    <!-- <section id="new-snippet-form" class="inline"></section> -->
-  </div>
-  <!-- Snippet list side page -->
-  <div v-if="showSnippetList" class="side-page w-full md:w-1/2">
-    <div class="flex justify-between items-center mb-6">
-      <h2 class="uppercase">Snippet List</h2>
-      <div class="action-buttons flex gap-2">
-        <button
-          class="bg-black text-white cursor-pointer text-sm self-end py-2 px-4 hover:bg-gray-800"
-          @click="toggleCreateSnippet"
-        >
-          <span class="md:hidden">Add </span>
-          <span class="hidden md:block">Add a Snippet</span>
-        </button>
-        <button
-          class="bg-black text-white cursor-pointer text-sm self-end py-2 px-4 hover:bg-gray-800"
-          @click="toggleSnippetList"
-        >
-          Close
-        </button>
+    <!-- Snippet list side page -->
+    <div v-if="showSnippetList" class="side-page w-full md:w-1/2">
+      <div class="mb-6 flex items-center justify-between">
+        <h2 class="uppercase">Snippet List</h2>
+        <div class="action-buttons flex gap-2">
+          <button
+            class="cursor-pointer self-end bg-black px-4 py-2 text-sm text-white hover:bg-gray-800"
+            @click="toggleCreateSnippet"
+          >
+            <span class="md:hidden">Add </span>
+            <span class="hidden md:block">Add a Snippet</span>
+          </button>
+          <button
+            class="cursor-pointer self-end bg-black px-4 py-2 text-sm text-white hover:bg-gray-800"
+            @click="toggleSnippetList"
+          >
+            Close
+          </button>
+        </div>
       </div>
-    </div>
 
-    <OrganismsSnippetList
-      class="overflow-y-auto h-full"
-      :snippets="snippets"
-      :inline-snippets="inlineSnippets"
-    />
-  </div>
-  <div v-if="showCreateSnippet" class="side-page w-full md:w-1/2">
-    <div class="flex justify-between items-center mb-6">
-      <h2 class="uppercase">Add a Snippet</h2>
-      <div class="action-buttons flex gap-2">
-        <button
-          class="bg-black text-white cursor-pointer text-sm self-end py-2 px-4 hover:bg-gray-800"
-          @click="toggleCreateSnippet"
-        >
-          Go back
-        </button>
+      <OrganismsSnippetList
+        class="h-full overflow-y-auto"
+        :snippets="snippets"
+        :inline-snippets="inlineSnippets"
+      />
+    </div>
+    <div v-if="showCreateSnippet" class="side-page w-full md:w-1/2">
+      <div class="mb-6 flex items-center justify-between">
+        <h2 class="uppercase">Add a Snippet</h2>
+        <div class="action-buttons flex gap-2">
+          <button
+            class="cursor-pointer self-end bg-black px-4 py-2 text-sm text-white hover:bg-gray-800"
+            @click="toggleCreateSnippet"
+          >
+            Go back
+          </button>
+        </div>
       </div>
-    </div>
 
-    <OrganismsNewSnippetForm
-      class="overflow-y-auto h-full"
-      :new-snippet-form="newSnippetForm"
-    />
+      <OrganismsNewSnippetForm
+        class="h-full overflow-y-auto"
+        :new-snippet-form="newSnippetForm"
+      />
+    </div>
   </div>
 </template>
 <style scoped>
