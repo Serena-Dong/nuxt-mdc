0ebcd5978189fb7d3adf0dc311d119f754ae92b4
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-05-14 11:17:51 +0200

    feat: format files with prettier

--- server/api/posts/index.get.ts
+++ server/api/posts/index.get.ts
@@ -1,33 +1,33 @@
-import getDB from "~~/server/utils/getDB";
+import getDB from '~~/server/utils/getDB'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async _event => {
   try {
-    const { db } = getDB();
+    const { db } = getDB()
 
-    await db.read();
-    const posts = db.data?.posts;
+    await db.read()
+    const posts = db.data?.posts
 
     if (!posts) {
       throw createError({
         statusCode: 404,
-        message: "Posts not found",
-        data: "No posts found in the database.",
-      });
+        message: 'Posts not found',
+        data: 'No posts found in the database.',
+      })
     }
 
-    return posts;
+    return posts
   } catch (error) {
     if (error instanceof Error) {
       throw createError({
         statusCode: (error as any).statusCode ?? 500,
-        statusMessage: "Error during /api/posts/ GET",
+        statusMessage: 'Error during /api/posts/ GET',
         data: (error as any).data ?? error.message,
-      });
+      })
     } else
       throw createError({
         statusCode: 500,
-        statusMessage: "Internal Server Error",
+        statusMessage: 'Internal Server Error',
         data: `${error}`,
-      });
+      })
   }
-});
+})
