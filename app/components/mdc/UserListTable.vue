cc7aed3f3893edf55ebb4c11f63d954da67f4b7f
Serena Dong <serena.dong@atoms.retex.com>
2025-05-20 09:49:21 +0200

    feat: add Suspense, UserListTable

--- app/components/mdc/UserListTable.vue
+++ app/components/mdc/UserListTable.vue
@@ -11,18 +11,20 @@ const { data: users } = await useFetch<UserData[]>(
 </script>
 
 <template>
-  <table v-if="users?.length" class="w-full">
-    <tr v-for="user in users" :key="user.username">
-      <td class="border-b-2 border-gray-400 py-2">
-        <p class="text-bold text-black">{{ user.name }}</p>
-      </td>
-      <td class="border-b-2 border-gray-400 py-2">
-        <p class="text-bold text-black">{{ user.username }}</p>
-      </td>
-      <td class="border-b-2 border-gray-400 py-2">
-        <p class="text-bold text-black">{{ user.email }}</p>
-      </td>
-    </tr>
-  </table>
-  <div v-else><p>UserListTable contains no users</p></div>
+  <Suspense suspensible>
+    <table v-if="users?.length" class="w-full">
+      <tr v-for="user in users" :key="user.username">
+        <td class="border-b-2 border-gray-400 py-2">
+          <p class="text-bold text-black">{{ user.name }}</p>
+        </td>
+        <td class="border-b-2 border-gray-400 py-2">
+          <p class="text-bold text-black">{{ user.username }}</p>
+        </td>
+        <td class="border-b-2 border-gray-400 py-2">
+          <p class="text-bold text-black">{{ user.email }}</p>
+        </td>
+      </tr>
+    </table>
+    <div v-else><p>UserListTable contains no users</p></div>
+  </Suspense>
 </template>
