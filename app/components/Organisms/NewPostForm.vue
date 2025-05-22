bf5cf734139903183d1c16c87527b5d7a076fdc6
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-05-22 17:19:53 +0200

    fix: fix TS null check

--- app/components/Organisms/NewPostForm.vue
+++ app/components/Organisms/NewPostForm.vue
@@ -119,7 +119,7 @@ const addNewLineOnEnter = (e: KeyboardEvent) => {
           class="mb-4 h-40 w-full overflow-y-scroll rounded border border-gray-300 p-2"
         >
           <MDC
-            v-if="(formValues.content ?? '').length"
+            v-if="formValues.content?.length"
             :key="formValues.content"
             class="markdown-content pointer-events-none"
             :value="formValues.content ?? ''"
